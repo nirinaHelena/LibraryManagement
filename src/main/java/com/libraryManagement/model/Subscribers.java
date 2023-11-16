d8ead89ae82e75bae97c662291349e912aa46347
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-16 17:19:25 +0300

    create model Subscribers

---
+++ src/main/java/com/libraryManagement/model/Subscribers.java
@@ -0,0 +1,7 @@
+package com.libraryManagement.model;
+
+public class Subscribers extends User{
+    public Subscribers(String idUser, String UserName, String UserReference) {
+        super(idUser, UserName, UserReference);
+    }
+}
