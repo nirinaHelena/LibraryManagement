d2cba54fc9cad89af4cb05bd5413f71b23f82596
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-16 17:20:35 +0300

    add args Constructor, getter, ... to User

--- src/main/java/com/libraryManagement/model/User.java
+++ src/main/java/com/libraryManagement/model/User.java
@@ -1,5 +1,13 @@
 package com.libraryManagement.model;
 
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class User {
     private String idUser;
     private String UserName;
