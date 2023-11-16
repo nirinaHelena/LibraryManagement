c1cf1f553bee497198796785b7247f51dec3db37
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-16 17:55:32 +0300

    add class Book

---
+++ src/main/java/com/libraryManagement/model/Book.java
@@ -0,0 +1,13 @@
+package com.libraryManagement.model;
+
+import java.text.DateFormat;
+
+public class Book {
+    private String id;
+    private String bookName;
+    private Author author;
+    private int pageNumbers;
+    private Topic topic;
+    private DateFormat realeseDate;
+    private Boolean available;
+}
