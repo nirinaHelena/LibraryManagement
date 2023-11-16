3b93297644e4a34643605a391a9851884c6f96b2
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-16 17:27:02 +0300

    add type Administrator

---
+++ src/main/java/com/libraryManagement/model/Administrator.java
@@ -0,0 +1,8 @@
+package com.libraryManagement.model;
+
+public class Administrator extends User{
+
+    public Administrator(String idUser, String UserName, String UserReference) {
+        super(idUser, UserName, UserReference);
+    }
+}
