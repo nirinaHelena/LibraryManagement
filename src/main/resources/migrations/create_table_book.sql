be68d3ffe1d37689f1d9b3481859460874e87855
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-18 11:25:47 +0300

    add script insert 3 datamock book

--- src/main/resources/migrations/create_table_book.sql
+++ src/main/resources/migrations/create_table_book.sql
@@ -7,3 +7,10 @@ create table if not exists book(
     realeaseDate date,
     is_available boolean not null
 );
+
+-- ajout de 3 lignes de données:
+insert into book (bookName, author_id, pageNumbers, topic, releaseDate, is_available)
+values
+('Titre du Livre 1', 1, 200, 'COMEDY', '2022-01-01', true),
+('Titre du Livre 2', 2, 150, 'ROMANCE', '2022-02-15', false),
+('Titre du Livre 3', 3, 300, 'OTHER', '2022-03-30', true);
