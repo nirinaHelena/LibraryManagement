f42ec2753311bc7f933c15f371c685a99e787904
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-18 12:44:41 +0300

    add script insert 3 datamock subscribers

--- src/main/resources/migrations/create_table_subscribers.sql
+++ src/main/resources/migrations/create_table_subscribers.sql
@@ -1,5 +1,12 @@
 create table if not exists subscribers(
     subscriberId serial primary key,
     subscriberName varchar not null,
-    subscriberReference varchar not null
+    subscriberReference varchar  not null unique
 );
+
+-- ajout de 3 lignes de données:
+insert into book (subscriberName)
+values
+('Titre du Livre 1'),
+('Titre du Livre 2'),
+('Titre du Livre 3');
