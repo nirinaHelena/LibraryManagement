af728843c808d4414e73017704214a2605606df2
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-19 16:18:56 +0300

    add constructor without args id

--- src/main/java/com/libraryManagement/model/Subscribers.java
+++ src/main/java/com/libraryManagement/model/Subscribers.java
@@ -2,6 +2,11 @@ package com.libraryManagement.model;
 
 
 public class Subscribers extends User{
+
+    public Subscribers(String userName, String userReference) {
+        super(userName, userReference);
+    }
+
     public Subscribers(String idUser, String UserName, String UserReference) {
         super(idUser, UserName, UserReference);
     }
