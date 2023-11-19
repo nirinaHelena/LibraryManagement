fae727325dd74bc98f5cb4e5011d5be42a70a183
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-19 10:43:36 +0300

    fix bug with connection in DatabaseConfig

--- src/main/java/com/libraryManagement/repository/DatabaseConfig.java
+++ src/main/java/com/libraryManagement/repository/DatabaseConfig.java
@@ -5,24 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private String url;
-    private String username;
-    private String password;
-
-    public DatabaseConfig(){
-        this.url = System.getenv("DB_URL");
-        this.username = System.getenv("DB_USER");
-        this.password = System.getenv("DB_PASSWORD");
-    }
-
-    public Connection createConnection(){
-        try{Connection connection = DriverManager.getConnection(
-                    this.url, this.username, this.password
-            );
-            return connection;
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return null;
+        private static final String URL= System.getenv("DB_URL");
+        private static final String USERNAME= System.getenv("DB_USER");
+        private static final String PASSWORD= System.getenv("DB_PASSWORD");
+    public  Connection createConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 }
