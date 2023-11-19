e504fd86b4e89d619359e4661f6f0dc15d1f5014
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-19 16:18:18 +0300

    add constructor without args id

--- src/main/java/com/libraryManagement/model/User.java
+++ src/main/java/com/libraryManagement/model/User.java
@@ -12,4 +12,9 @@ public class User {
     private String idUser;
     private String UserName;
     private String UserReference;
+
+    public User( String userName, String userReference) {
+        UserName = userName;
+        UserReference = userReference;
+    }
 }
