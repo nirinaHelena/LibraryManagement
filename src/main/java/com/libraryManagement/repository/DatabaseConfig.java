c77510bddae50844b0b2bbcb6e10da7960e9f7f7
nirinaHelena <127594383+nirinaHelena@users.noreply.github.com>
2023-11-19 21:51:08 +0300

    fix bug

--- src/main/java/com/libraryManagement/repository/DatabaseConfig.java
+++ src/main/java/com/libraryManagement/repository/DatabaseConfig.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-        private static final String URL= "jdbc:postgresql://localhost/library_management";
-        private static final String USERNAME= "prog_admin";
-        private static final String PASSWORD= "123456";
+        private static final String URL= System.getenv("DB_URL");
+        private static final String USERNAME= System.getenv("DB_USERNAME");
+        private static final String PASSWORD= System.getenv("DB_PASSWORD");
     public  Connection createConnection(){
         try {
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
