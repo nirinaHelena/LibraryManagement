4ada10e519f03c27084f9f42332d0467c5db05db
nirinaHelena <hei.nirina.7@gmail.com>
2023-11-19 15:56:09 +0300

    add constructor without args id

--- src/main/java/com/libraryManagement/model/Author.java
+++ src/main/java/com/libraryManagement/model/Author.java
@@ -11,4 +11,9 @@ public class Author {
     private String id;
     private String authorName;
     private Character sex;
+
+    public Author( String authorName, Character sex) {
+        this.authorName = authorName;
+        this.sex = sex;
+    }
 }
